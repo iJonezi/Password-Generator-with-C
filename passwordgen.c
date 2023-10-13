186860c6c1dac0599104d1d3722ea84bfc7262e2
iJonezi <jonescarson08@gmail.com>
2023-10-12 19:53:15 -0800

    Create Program Layout
    -Added letternumbspecgen function
    -Added letternumbgen function
    -Added lettergen function
    -Added numbergen function
    -Added directory function
    -stdlib.h included

--- passwordgen.c
+++ passwordgen.c
@@ -2,8 +2,20 @@
 #include <math.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-bool letter = false;
+// initializing values
+bool letter;
+bool number;
+bool special;
+bool directory;
+
+// initialize functions
+int letternumbspecgen(void);
+int letternumbergen(void);
+int lettergen(void);
+int numbergen(void);
+void directory(char *path);
 
 int main(void)
 {
@@ -17,20 +29,104 @@ int main(void)
 
     if (strcmp(word, "y") == 0 || strcmp(word, "yes") == 0)
     {
-        printf("Letters will be used!");
+        printf("Letters will be used!\n");
         letter = true; // yes
-        return 0;
+        word[0] = '\0';
     }
     else if (strcmp(word, "n") == 0 || strcmp(word, "no") == 0)
     {
-        printf("Letters will not be used!");
+        printf("Letters will not be used!\n");
         letter = false; // no
-        return 0;
+        word[0] = '\0';
     }
     else
     {
         printf("Error, try again, please reply with either a Yes or No\n");
         return 1; // did not say yes or no
     }
+
+    printf("Would you like to use Numbers? (y/n): ");
+    scanf("%s", &word);
+
+    if (strcmp(word, "y") == 0 || strcmp(word, "yes") == 0)
+    {
+        printf("Numbers will be used!\n");
+        number = true; // yes
+        word[0] = '\0';
+    }
+    else if (strcmp(word, "n") == 0 || strcmp(word, "no") == 0)
+    {
+        printf("Numbers will not be used!\n");
+        number = false; // no
+        word[0] = '\0';
+    }
+    else
+    {
+        printf("Error, try again, please reply with either a Yes or No\n");
+        return 1; // did not say yes or no
+    }
+
+    printf("Would you like to use Special Characters? (@, #, $, etc) (y/n): ");
+    scanf("%s", &word);
+
+    if (strcmp(word, "y") == 0 || strcmp(word, "yes") == 0)
+    {
+        printf("Special Characters will be used!\n");
+        special = true; // yes
+        word[0] = '\0';
+    }
+    else if (strcmp(word, "n") == 0 || strcmp(word, "no") == 0)
+    {
+        printf("Special Characters will not be used!\n");
+        special = false; // no
+        word[0] = '\0';
+    }
+    else
+    {
+        printf("Error, try again, please reply with either a Yes or No\n");
+        return 1; // did not say yes or no
+    }
+
+    if (letter && number)
+    {
+        letternumbgen();
+    }
+    if (letter && !number)
+    {
+        lettergen();
+    }
+    if (!letter && number)
+    {
+        numbergen();
+    }
+
     return 0;
+}
+// generate if letter + number + special combiniation
+int letternumbspecgen(void)
+{
+    return;
+}
+
+// generate if letter + number combination
+int letternumbgen(void)
+{
+    return;
+}
+// generate if letter only
+int lettergen(void)
+{
+    return;
+}
+// generate if number only
+int numbergen(void)
+{
+    return;
+}
+
+void directory(char *path)
+{
+    // char *dir = malloc(path);
+    // free(dir);
+    return;
 }
