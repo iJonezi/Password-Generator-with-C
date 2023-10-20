bd6226ce3bf8d15edbbe4a2c50a257a14415bb44
iJonezi <jonescarson08@gmail.com>
2023-10-19 16:36:23 -0800

    Completed Letter Generator with Maximum of 10 Char

--- passwordgen.c
+++ passwordgen.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 // initializing values
 bool letter;
@@ -10,19 +11,30 @@ bool number;
 bool special;
 bool directory;
 
+char letters[] = "abcdefghijklmnopqrstuvwxyz";
+char numbers[] = "0123456789";
+char specials[] = "!@#$^&*?";
+
 // initialize functions
-int letternumbspecgen(void);
-int letternumbergen(void);
-int lettergen(void);
-int numbergen(void);
-void directory(char *path);
+int letternumbspecgen(int count);
+int letternumbergen(int count);
+int lettergen(int count);
+int numbergen(int count);
+void savepath(char *path);
+
+char password[10];
 
 int main(void)
 {
+    srand((unsigned int)(time(NULL)));
+
+    int count = 0;
     // greet user and prompt first question
     printf("Let's Make a Password!\n");
     // establish memory for responses
     char word[4];
+    char len[2];
+    // variable for length
     // ask user if they want letters
     printf("Would you like to use Letters? (y/n): ");
     scanf("%s", &word);
@@ -87,46 +99,71 @@ int main(void)
         return 1; // did not say yes or no
     }
 
+    printf("How long would you like this password to be? (0 - 10): ");
+    scanf("%s", &len);
+
+    if (strcmp(len, "0") == 0 || strcmp(len, "1") == 0 || strcmp(len, "2") == 0 ||
+        strcmp(len, "3") == 0 || strcmp(len, "4") == 0 || strcmp(len, "5") == 0 ||
+        strcmp(len, "6") == 0 || strcmp(len, "7") == 0 || strcmp(len, "8") == 0 ||
+        strcmp(len, "9") == 0 || strcmp(len, "10") == 0)
+    {
+        printf("Creating a password with %s characters...\n", len);
+        int number = atoi(len);
+        printf("The number is %i\n", number);
+        count = number;
+        len[0] = '\0';
+    }
+    else
+    {
+        printf("Error, try again, please include a number from 0 to 10\n");
+        return 1; // did not supply a number
+    }
+
     if (letter && number)
     {
-        letternumbgen();
+        letternumbergen(count);
     }
     if (letter && !number)
     {
-        lettergen();
+        lettergen(count);
     }
     if (!letter && number)
     {
-        numbergen();
+        numbergen(count);
     }
 
     return 0;
 }
+
 // generate if letter + number + special combiniation
-int letternumbspecgen(void)
+int letternumbspecgen(int count)
 {
-    return;
+    return 0;
 }
 
 // generate if letter + number combination
-int letternumbgen(void)
+int letternumbergen(int count)
 {
-    return;
+    return 0;
 }
 // generate if letter only
-int lettergen(void)
+int lettergen(int count)
 {
-    return;
+    int i = 0;
+    for (i = 0; i < count; i++)
+    {
+        password[i] = letters[rand() % 26];
+        printf("%c", password[i]);
+    }
 }
 // generate if number only
-int numbergen(void)
+int numbergen(int count)
 {
-    return;
+    return 0;
 }
 
-void directory(char *path)
+void savepath(char *path)
 {
     // char *dir = malloc(path);
     // free(dir);
-    return;
 }
